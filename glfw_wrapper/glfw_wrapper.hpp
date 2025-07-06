16919f002a65129aa6b0eae1039c0443e856caab
Neutron12138 <121910770+Neutron12138@users.noreply.github.com>
2025-07-06 11:27:19 +0800

    Delete glfw_wrapper directory

--- glfw_wrapper/glfw_wrapper.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "base/error.hpp"
-#include "base/glfw.hpp"
-#include "base/error_callback.hpp"
-#include "base/context.hpp"
-
-#include "window/window.hpp"
-#include "window/window.cpp"
